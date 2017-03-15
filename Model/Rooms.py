507466529fd64e688e894487a983528478d4e7c8
dennomwas <githinji.mwangi@gmail.com>
2017-03-15 18:02:26 +0300

    [chore]Refactor to rooms

--- Model/Rooms.py
+++ Model/Rooms.py
@@ -9,19 +9,26 @@ class Office(Room):
     max_capacity = 6
 
     def __init__(self, room_name):
-        self.available_spaces = 6
+        # self.available_spaces = 6
         super(Office, self).__init__(room_name)
         self.room_type = 'office'
         self.persons_allocated = []
+
+    def __str__(self):
+        return self.room_name + ' ' + self.room_type + ' ' + str(self.persons_allocated)
 
 
 class LivingSpace(Room):
     max_capacity = 4
 
     def __init__(self, room_name):
-        self.available_spaces = 4
+        # self.available_spaces = 4
         super(LivingSpace, self).__init__(room_name)
         self.room_type = 'livingspace'
         self.persons_allocated = []
 
+    def __str__(self):
+        return self.room_name + ' ' + self.room_type + ' ' + str(self.persons_allocated)
 
+d = Office("narnia")
+print(d)
