b7ed7740b3f38e70a9d0bad34373d7a14e9648fb
dennomwas <githinji.mwangi@gmail.com>
2017-03-21 09:40:42 +0300

    [Chore]Refactor the logic

--- Model/People.py
+++ Model/People.py
@@ -14,15 +14,27 @@ class Staff(Person):
         self.current_occupancy = []
         self.identifier = id(self)
 
+    def __str__(self):
+        return str(self.identifier) + ' ' + self.first_name + ' ' + self.last_name + ' ' + self.designation + str(self.identifier)
 
 class Fellow(Person):
 
-    def __init__(self, first_name, last_name, accommodation_request='n'):
+    def __init__(self, first_name, last_name, accommodation_request=None):
         super(Fellow, self).__init__(first_name, last_name)
         self.accommodation_request = accommodation_request
         self.designation = 'Fellow'
         self.current_occupancy = []
         self.identifier = id(self)
+
+    def __str__(self):
+        return str(self.identifier) + ' ' + self.first_name + " " +  self.last_name + " " + self.designation
+
+
+#
+# d = Fellow('Dennis', 'Mwangi')
+# print(d)
+
+
 
 
 
