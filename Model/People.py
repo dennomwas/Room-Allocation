a8fc6847308b6209d77e451ffb181887e5fffb01
dennomwas <githinji.mwangi@gmail.com>
2017-03-29 16:21:47 +0300

    [Feature] Add abstraction to class

--- Model/People.py
+++ Model/People.py
@@ -1,58 +1,38 @@
+from abc import ABCMeta, abstractmethod
 
-class Person(object):
 
+class Person(metaclass=ABCMeta):
+
+    @abstractmethod
     def __init__(self, first_name, last_name, current_office=None):
         self.first_name = first_name
         self.last_name = last_name
         self.current_office = current_office
+        self.identifier = id(self)
 
 
 class Staff(Person):
 
-    def __init__(self, first_name, last_name, current_occupancy=None):
+    def __init__(self, *args, **kwarg):
 
-        super(Staff, self).__init__(first_name, last_name, current_occupancy)
-        self.identifier = id(self)
-
+        super(Staff, self).__init__(*args, **kwarg)
+        self.person_type = "STAFF"
 
     def __str__(self):
-        return '{} {} {}'.format(self.identifier, self.first_name, self.last_name)
+        return '{} {} {} {}'.format(self.identifier, self.first_name,
+                                    self.last_name, self.person_type)
 
-    # def __repr__(self):
-    #     return str(self.identifier) + ' ' + self.first_name + ' ' + self.last_name
 
 class Fellow(Person):
 
-    def __init__(self, first_name, last_name,
-                 current_occupancy=None, accommodation_request=None):
+    def __init__(self, *args, accommodation_request=None):
 
-        super(Fellow, self).__init__(first_name, last_name)
+        super(Fellow, self).__init__(*args)
         self.accommodation_request = accommodation_request
-        self.identifier = id(self)
         self.current_living = None
+        self.person_type = "FELLOW"
 
     def __str__(self):
-        return '{} {} {}'.format(self.identifier, self.first_name,
-                                    self.last_name)
-
-    #
-    # def __repr__(self):
-    #     return str(self.identifier) + ' ' + self.first_name + ' ' + self.last_name
-
-
-
-# d = Fellow('Dennis', 'Mwangi')
-# print(d)
-#
-# d = Staff('Dennis', 'Mwangi')
-#
-# print(d)
-
-
-
-
-
-
-
-
+        return '{} {} {} {}'.format(self.identifier, self.first_name,
+                                    self.last_name, self.person_type)
 
